d509179ffcbbf8cbf882ab48405c21166d8e21ae
Centrix14 <artiom.zvyagin@yandex.ru>
2021-01-20 17:59:04 +0300

    removed dependences

--- list.c
+++ list.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 #include "list.h"
-#include "../figure/figure.h"
-#include "../dbg.h"
 
 list *list_init_node(list *parent) {
 	list *nptr = malloc(sizeof(list));
@@ -23,12 +21,8 @@ list *list_init_node(list *parent) {
 }
 
 void list_free_node(list *node) {
-	if (node) {
-#ifdef DBG
-		printf("*** free: %p\n", node);
-#endif
+	if (node)
 		free(node);
-	}
 }
 
 void list_add_node(list *parent) {
@@ -79,17 +73,4 @@ void *list_get_data(list *lptr) {
 	if (lptr && lptr->data)
 		return lptr->data;
 	return NULL;
-}
-
-void list_dump_node(list *lptr) {
-	FILE *file = fopen("dump", "w");
-	figure *fptr = NULL;
-
-	fptr = list_get_data(lptr);
-	if (!fptr)
-		return ;
-	
-	fprintf(stdout, "id = %s\tme = %p\tprev = %p\tnext = %p\tvm = %d (need 4)\n", fptr->id, lptr, lptr->prev, lptr->next, fptr->visible);
-
-	fclose(file);
 }
